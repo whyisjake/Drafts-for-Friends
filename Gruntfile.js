b47ead10659c19ea25e5764d1496103816bb577d
Jake Spurlock <jspurlock@makermedia.com>
2014-04-23 08:38:05 -0700

    Updated the Grunt file with a few more tasks.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,13 @@ module.exports = function(grunt) {
 
 		pkg: grunt.file.readJSON( 'package.json' ),
 
+		po2mo: {
+			files: {
+				src: 'languages/*.po',
+				expand: true,
+			},
+		},
+
 		pot: {
 			options:{
 				text_domain: 'drafts-for-friends',
@@ -27,10 +34,45 @@ module.exports = function(grunt) {
 				],
 			},
 			files:{
-				src:  [ 'drafts-for-friends.php' ], //Parse all php files
+				src: [
+					'drafts-for-friends.php',
+					'drafts-for-friends-admin-page.php'
+					],
 				expand: true,
 			}
 		},
+
+		checktextdomain: {
+			options:{
+				text_domain: 'drafts-for-friends',
+				correct_domain: true, // Will correct missing/variable domains
+				keywords: [ // WordPress localisation functions
+					'__:1,2d',
+					'_e:1,2d',
+					'_x:1,2c,3d',
+					'esc_html__:1,2d',
+					'esc_html_e:1,2d',
+					'esc_html_x:1,2c,3d',
+					'esc_attr__:1,2d',
+					'esc_attr_e:1,2d',
+					'esc_attr_x:1,2c,3d',
+					'_ex:1,2c,3d',
+					'_n:1,2,4d',
+					'_nx:1,2,4c,5d',
+					'_n_noop:1,2,3d',
+					'_nx_noop:1,2,3c,4d'
+				],
+			},
+			files: {
+				src:  [ '**/*.php', ], //All php files
+				expand: true,
+			},
+		},
+
 	});
+
 	grunt.loadNpmTasks('grunt-pot');
+	grunt.loadNpmTasks('grunt-po2mo');
+	grunt.loadNpmTasks('grunt-checktextdomain');
+
 };
